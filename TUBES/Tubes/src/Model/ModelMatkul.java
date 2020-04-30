c306d6adc241032fd373f0bc7e8bff6a350aedbe
Rara El-Ghiffari <49091818+El-Ghiffari@users.noreply.github.com>
2020-04-30 15:12:52 +0700

    Update ModelMatkul.java

--- TUBES/Tubes/src/Model/ModelMatkul.java
+++ TUBES/Tubes/src/Model/ModelMatkul.java
@@ -13,74 +13,114 @@ import java.util.ArrayList;
  *
  * @author R O G
  */
-public class ModelMatkul {
-    private String KD_MK;
-    private String nama_MK;
-    private String SKS;
-    private ArrayList<ModelMahasiswa> Mhs;
-    private ModelDosen Dsn;
-//    String sql;
-//    Statement stat;
+public class Matkul {
+    private Database db;
+    private String sql;
+    private Statement stat;
+    private ArrayList<ModelMatkul> listMatkul = new ArrayList();
     
+    public class ModelMatkul {
+        private String KD_MK;
+        private String nama_MK;
+        private String SKS;
+        private Dosen Dsn;
+        private ArrayList<ModelMahasiswa> listMhs;
+        
+        public ModelMatkul(String KD_MK, String nama_MK, String SKS) {
+            this.KD_MK = KD_MK;
+            this.nama_MK = nama_MK;
+            this.SKS = SKS;
+            listMhs = new ArrayList();
+        }
+
+        public String getKD_MK() {
+            return KD_MK;
+        }
+
+        public void setKD_MK(String KD_MK) {
+            this.KD_MK = KD_MK;
+        }
+
+        public String getNama_MK() {
+            return nama_MK;
+        }
+
+        public void setNama_MK(String nama_MK) {
+            this.nama_MK = nama_MK;
+        }
+
+        public String getSKS() {
+            return SKS;
+        }
+
+        public void setSKS(String SKS) {
+            this.SKS = SKS;
+        }
+
+        public Dosen getDsn() {
+            return Dsn;
+        }
+
+        public void setDsn(Dosen Dsn) {
+            this.Dsn = Dsn;
+        }
+    }
     
-    public ModelMatkul(String KD_MK, String nama_MK, String SKS) {
-        this.KD_MK = KD_MK;
-        this.nama_MK = nama_MK;
-        this.SKS = SKS;
+     public Matkul (Database a) {
+        db = a;
+        try {
+            db.connect();
+            sql = "SELECT * FROM matkul";
+            db.setRs(db.getStmt().executeQuery(sql));
+            ModelMatkul m;
+            while (db.getRs().next()) {
+                m = new ModelMatkul(
+                    db.getRs().getString("KD_MK"),
+                    db.getRs().getString("nama_MK"),
+                    db.getRs().getString("SKS")
+                );
+                listMatkul.add(m);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();;
+        }
     }
-
-    public String getKD_MK() {
-        return KD_MK;
-    }
-
-    public void setKD_MK(String KD_MK) {
-        this.KD_MK = KD_MK;
-    }
-
-    public String getNama_MK() {
-        return nama_MK;
-    }
-
-    public void setNama_MK(String nama_MK) {
-        this.nama_MK = nama_MK;
-    }
-
-    public String getSKS() {
-        return SKS;
-    }
-
-    public void setSKS(String SKS) {
-        this.SKS = SKS;
-    }
-
-    public ModelDosen getDsn() {
-        return Dsn;
-    }
-
-    public void setDsn(ModelDosen Dsn) {
-        this.Dsn = Dsn;
-    }
-
-//    public void addMahasiswa(ModelMahasiswa mhs) {
-//        this.Mhs.add(mhs);
-//    }
-//    
-//    public void delMahasiswa(ModelMahasiswa mhs) {
-//        this.Mhs.remove(mhs);
-//    }
     
-//    public boolean addMatkul(String KD_MK, String Nama_MK, String SKS) {
-//        boolean value = false;
-//        System.out.println(value);
-//        try{            
-//            sql = "INSERT INTO matakuliah VALUES ('"+KD_MK+"','"+Nama_MK+"','"+SKS+"')";
-//            stat.executeUpdate(sql);            
-//            value = true;
-//        } catch (Exception e) {
-//            JOptionPane.showMessageDialog(null, "Penyimpanan Gagal, "+e.getMessage());
-//        }
-//        
-//        return value;
-//    
-//    }
+    public void addMatkul(ModelMatkul m){
+        //boolean value = false;
+        try{   
+            db.connect();
+            sql = "INSERT INTO matkul VALUES('" + m.getKD_MK()+ "','" + m.getNama_MK() +"','"+ m.getSKS() +"')";
+            db.setRs(db.getStmt().executeQuery(sql));
+            db.disconnect();
+            listMatkul.add(m);
+            //value = true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Penyimpanan Gagal, "+e.getMessage());
+        }
+        //return value;
+    }
+    
+    public void deleteMatkul(ModelMatkul m){
+        //boolean value = false;
+        try{   
+            db.connect();
+            sql = "DELETE FROM matkul WHERE KD_MK = '" + m.getKD_MK() + "'";
+            db.setRs(db.getStmt().executeQuery(sql));
+            db.disconnect();
+            listMatkul.remove(m);
+            //value = true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Delete Gagal, "+e.getMessage());
+        }
+        //return value;
+    }
+    
+     public ArrayList<ModelMatkul> getListMatkul() {
+        return listMahatkul;
+    }
+
+    public void setListMatkul(ArrayList<ModelMatkul> listMatkul) {
+        this.listMatkul = listMatkul;
+    }
 }

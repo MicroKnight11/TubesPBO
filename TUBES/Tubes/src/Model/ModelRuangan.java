e492574b35507aded9d95a1588667ac1c598efb4
Deni Saputra Hermawan <denisaputrah123@gmail.com>
2020-04-30 15:34:38 +0700

    Ruangan

--- TUBES/Tubes/src/Model/ModelRuangan.java
+++ TUBES/Tubes/src/Model/ModelRuangan.java
@@ -5,6 +5,8 @@
  */
 package tubes;
 
+import Model.Database;
+import Model.Matkul;
 import java.sql.Statement;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
@@ -23,19 +25,39 @@ public class ModelRuangan extends ModelGedung{
     int id;
     private ArrayList<ModelRuangan> listRuang = new ArrayList();
     
-    public ModelRuangan(String KD_GD, String nama_GD, String KD_Ruangan) {
-        super(KD_GD, nama_GD);
-        this.KD_Ruangan = KD_Ruangan;
-    }
+    public class Ruangan{
+       private String KD_Ruangan;
+       public Ruangan(String KD_GD, String nama_GD, String KD_Ruangan) {
+            super(KD_GD, nama_GD);
+            this.KD_Ruangan = KD_Ruangan;
+        }
 
-    public String getKD_Ruangan() {
-        return KD_Ruangan;
-    }
+        public String getKD_Ruangan() {
+            return KD_Ruangan;
+        }
 
-    public void setKD_Ruangan(String KD_Ruangan) {
-        this.KD_Ruangan = KD_Ruangan;
+        public void setKD_Ruangan(String KD_Ruangan) {
+            this.KD_Ruangan = KD_Ruangan;
+        } 
     }
     
+    public ModelRuangan(Database a) {
+        db = a;
+        try {
+            db.connect();
+            sql = "SELECT * FROM ruangan";
+            db.setRs(db.getStmt().executeQuery(sql));
+            Ruangan m;
+            while (db.getRs().next()) {
+                m = new Ruangan(
+                    db.getRs().getString("KD_Ruangan"),
+                );
+                listRuang.add(m);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();;
+        }
+    }
      public boolean addRuang(String kodeGedung, String namaGedung , String kodeRuangan){
         boolean value = false;
 

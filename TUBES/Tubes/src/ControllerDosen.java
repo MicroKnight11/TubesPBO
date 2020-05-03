962bbea1da9570949efa4f51c1d89ca9f210a200
Rara El-Ghiffari <49091818+El-Ghiffari@users.noreply.github.com>
2020-05-03 21:01:02 +0700

    Update ControllerDosen.java

--- TUBES/Tubes/src/ControllerDosen.java
+++ TUBES/Tubes/src/ControllerDosen.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tubes;
+package Controller;
 
 //import View.MainGUIDosen;
 //import View.MainGUIhome;

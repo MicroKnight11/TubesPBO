10f583c2010dbe9c4f163e7040fc55b409754b8f
Satria <48939942+MicroKnight11@users.noreply.github.com>
2020-05-04 09:08:17 +0700

    edit dikit controller dosen

--- TUBES/Tubes/src/Controller/ControllerDosen.java
+++ TUBES/Tubes/src/Controller/ControllerDosen.java
@@ -2,9 +2,13 @@ package Controller;
 
 
 import Model.Database;
+import Model.ModelDosen;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import tubes.MainGUIDosen;
 
 /*
@@ -18,16 +22,16 @@ import tubes.MainGUIDosen;
  * @author satria
  */
 public class ControllerDosen implements ActionListener{
+    private MainGUIDosen viewDosen;           
     Database db;
-    //Dosen dosen;  
-    //private ArrayList<Dosen> listDosen = new ArrayList();
-    ResultSet rs;
+    private ModelDosen dosen;  
+    private ArrayList<ModelDosen> listDosen = new ArrayList();
     
-    public ControllerDosen(Dosen dsn) {
+    public ControllerDosen(Database db) {
         viewDosen = new MainGUIDosen();
         viewDosen.setVisible(true);
         viewDosen.addActionListener(this);    
-        db = new Database();
+        this.db = db
         this.dosen = dsn;
         getlistData();
         viewDosen.setDataDosen(listDosen);
@@ -35,13 +39,15 @@ public class ControllerDosen implements ActionListener{
     
     public void getlistData(){
         try{
-            ResultSet rs = db.getData("select * from dosen");
+            db.connect();
+            String sql = "select * from dosen";
+            db.setRs(db.getStmt().executeQuery(sql));
             listDosen.removeAll(listDosen);
-            while(rs.next()){
-                Dosen dsn = new Dosen(rs.getString("NIDN"), rs.getString("Name"));
-                listDosen.add(dsn);
-                        
+            while(db.getRs().next()){
+                ModelDosen dsn = new ModelDosen(db.getRs().getString("NIDN"), db.getRs().getString("FirstName"), db.getRs().getString("LastName"));
+                listDosen.add(dsn);          
             }
+            db.disconnect();
         }catch(Exception e){
             System.out.println("Error refresh"+e.getMessage());
         }
@@ -60,16 +66,22 @@ public class ControllerDosen implements ActionListener{
                     if (viewDosen.getjTextFieldNID().getText().equals("")){
                         JOptionPane.showMessageDialog(null, "NIDN harus diisi terlebih dahulu");
                     } else {
+                        db.connect();
                         String NID = viewDosen.getjTextFieldNID().getText();
-                        String dsn = "SELECT FROM dosen WHERE NIDN ='"+NID+"'";
-                        viewDosen.setDataDosen(db.query(dsn));
+                        String sql = "SELECT * FROM mata_kuliah NATURAL JOIN jadwal WHERE NID ='"+NID+"'";
+                        db.setRs(db.getStmt().executeQuery(sql));
+                        while(db.getRs().next()){
+                            ModelDosen dsn1 = new ModelDosen(rs.getString("NIDN"), rs.getString("FirstName"), rs.getString("LastName"));
+                            listDosen.add(dsn1);          
+                        }
+                        viewDosen.setDataDosen(listDosen);
                     }
                 } catch (Exception es) {
                     System.out.println("Error 404 "+ es.getMessage());
                     JOptionPane.showMessageDialog(null, "Data Dosen Tidak DItemukan");
                 }
             }
-        } catch (Exception e) {
+        } catch (Exception ef) {
             JOptionPane.showMessageDialog(null, "Data Dosen Tidak DItemukan");
         }
     }
